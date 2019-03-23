df20571ed75c98d0c2b3f1e1eb46b1ea402f6b6d
Vadim Sokoltsov <vforvad@gmail.com>
2019-03-23 18:29:16 +0300

    Add C homework

---
+++ 13_c/tests/test_pb.py
@@ -0,0 +1,36 @@
+import os
+import unittest
+
+import pb
+MAGIC = 0xFFFFFFFF
+DEVICE_APPS_TYPE = 1
+TEST_FILE = "test.pb.gz"
+
+
+class TestPB(unittest.TestCase):
+    deviceapps = [
+        {"device": {"type": "idfa", "id": "e7e1a50c0ec2747ca56cd9e1558c0d7c"},
+         "lat": 67.7835424444, "lon": -22.8044005471, "apps": [1, 2, 3, 4]},
+        {"device": {"type": "gaid", "id": "e7e1a50c0ec2747ca56cd9e1558c0d7d"}, "lat": 42, "lon": -42, "apps": [1, 2]},
+        {"device": {"type": "gaid", "id": "e7e1a50c0ec2747ca56cd9e1558c0d7d"}, "lat": 42, "lon": -42, "apps": []},
+        {"device": {"type": "gaid", "id": "e7e1a50c0ec2747ca56cd9e1558c0d7d"}, "apps": [1]},
+    ]
+
+    def tearDown(self):
+        os.remove(TEST_FILE)
+
+    def test_write(self):
+        bytes_written = pb.deviceapps_xwrite_pb(self.deviceapps, TEST_FILE)
+        self.assertTrue(bytes_written > 0)
+        with gzip.open(TEST_FILE) as gz_file:
+            for _ in range(len(self.deviceapps)):
+                magic, dev_type, length = unpack("<Ihh", gz_file.read(HEADER_SIZE))
+                self.assertEqual(magic, MAGIC)
+                self.assertEqual(dev_type, DEVICE_APPS_TYPE)
+                _ = gz_file.read(length)
+
+    @unittest.skip("Optional problem")
+    def test_read(self):
+        pb.deviceapps_xwrite_pb(self.deviceapps, TEST_FILE)
+        for i, d in enumerate(pb.deviceapps_xread_pb(TEST_FILE)):
+            self.assertEqual(d, self.deviceapps[i])
